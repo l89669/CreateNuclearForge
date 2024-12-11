81fc6cc3e883279556dfa99cf0641cdd7d42c6a8
injin0301 <injinkim0808@gmail.com>
2024-12-11 09:39:37 +0100

    update

---
+++ src/main/java/net/nuclearteam/createnuclear/CreateNuclear.java
@@ -0,0 +1,59 @@
+package net.nuclearteam.createnuclear;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import net.minecraftforge.fml.common.Mod;
+import com.mojang.logging.LogUtils;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.resources.ResourceKey;
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipModifier;
+import com.simibubi.create.foundation.item.TooltipHelper.Palette;
+import net.minecraft.resources.ResourceLocation;
+
+@Mod(CreateNuclear.MOD_ID)
+public class CreateNuclear {
+    public static final String MOD_ID = "createnuclear";
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MOD_ID)
+            .defaultCreativeTab((ResourceKey<CreativeModeTab>) null);
+
+    static {
+        REGISTRATE.setTooltipModifierFactory(item -> {
+            return new ItemDescription.Modifier(item, Palette.STANDARD_CREATE)
+                    .andThen(TooltipModifier.mapNull(KineticStats.create(item)));
+        });
+    }
+
+    public CreateNuclear() {
+        onInitialize();
+    }
+
+    public static void onInitialize() {
+        ModLoadingContext modLoadingContext = ModLoadingContext.get();
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get()
+                .getModEventBus();
+        IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+
+        REGISTRATE.registerEventListeners(modEventBus);
+
+
+    }
+
+    public static void init(final FMLCommonSetupEvent event) {
+
+    }
+
+
+    public static ResourceLocation asResource(String path) {
+        return new ResourceLocation(MOD_ID, path);
+    }
+
+}

49e03c10d5a77e135f04745d4bb3b271c39f6dcc
injin0301 <injinkim0808@gmail.com>
2024-12-11 16:26:05 +0100

    update no error

--- src/main/java/net/nuclearteam/createnuclear/CreateNuclear.java
+++ src/main/java/net/nuclearteam/createnuclear/CreateNuclear.java
@@ -1,32 +1,33 @@
 package net.nuclearteam.createnuclear;
 
+
+import com.mojang.logging.LogUtils;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipHelper;
+import com.simibubi.create.foundation.item.TooltipModifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-import net.minecraftforge.fml.common.Mod;
-import com.mojang.logging.LogUtils;
-import com.simibubi.create.foundation.data.CreateRegistrate;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.resources.ResourceKey;
-import com.simibubi.create.foundation.item.ItemDescription;
-import com.simibubi.create.foundation.item.KineticStats;
-import com.simibubi.create.foundation.item.TooltipModifier;
-import com.simibubi.create.foundation.item.TooltipHelper.Palette;
-import net.minecraft.resources.ResourceLocation;
 
 @Mod(CreateNuclear.MOD_ID)
 public class CreateNuclear {
     public static final String MOD_ID = "createnuclear";
     public static final Logger LOGGER = LogUtils.getLogger();
     public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MOD_ID)
-            .defaultCreativeTab((ResourceKey<CreativeModeTab>) null);
+           .defaultCreativeTab((ResourceKey<CreativeModeTab>) null);
 
     static {
         REGISTRATE.setTooltipModifierFactory(item -> {
-            return new ItemDescription.Modifier(item, Palette.STANDARD_CREATE)
+            return new ItemDescription.Modifier(item, TooltipHelper.Palette.STANDARD_CREATE)
                     .andThen(TooltipModifier.mapNull(KineticStats.create(item)));
         });
     }

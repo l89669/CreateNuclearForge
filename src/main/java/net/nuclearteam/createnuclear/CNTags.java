d9faf1ca7109916fc497036b6f59af78b03151c2
Giovanni <giovanni.ricotta@ynov.com>
2024-12-30 14:47:21 +0100

    initial-create-nuclear-forge: Updated fini

--- src/main/java/net/nuclearteam/createnuclear/CNTags.java
+++ src/main/java/net/nuclearteam/createnuclear/CNTags.java
@@ -1,22 +1,27 @@
 package net.nuclearteam.createnuclear;
 
 import com.simibubi.create.foundation.utility.Lang;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 
-import static net.nuclearteam.createnuclear.CNTags.NameSpace.MOD;
-
 import java.util.Collections;
+
+import static net.nuclearteam.createnuclear.CNTags.NameSpace.*;
 
 public class CNTags {
     public static <T> TagKey<T> optionalTag(IForgeRegistry<T> registry, ResourceLocation id) {
@@ -24,7 +29,7 @@ public class CNTags {
     }
 
     public static <T> TagKey<T> forgeTag(IForgeRegistry<T> registry, String path) {
-        return optionalTag(registry, new ResourceLocation("forge", path));
+        return optionalTag(registry, new ResourceLocation(FORGE.id, path));
     }
 
     public static TagKey<Block> forgeBlockTag(String path) {
@@ -62,6 +67,18 @@ public class CNTags {
     }
 
     public enum CNBlockTags {
+        FAN_PROCESSING_CATALYSTS_ENRICHED(MOD, "fan_processing_catalysts/enriched"),
+        ENRICHING_FIRE_BASE_BLOCKS,
+        CAMPFIRE(MINECRAFT),
+        ALL_CAMPFIRES(MINECRAFT, "all/campfires"),
+        DRAGON_TRANSPARENT(MINECRAFT),
+        FIRE(MINECRAFT),
+        NEEDS_DIAMOND_TOOL(MINECRAFT),
+        NEEDS_IRON_TOOL(MINECRAFT),
+        NEEDS_STONE_TOOL(MINECRAFT),
+        SHOVEL(MINECRAFT, "mineable/shovel"),
+        URANIUM_ORES,
+        LEAD_ORES
         ;
 
         public final TagKey<Block> tag;
@@ -110,6 +127,11 @@ public class CNTags {
     }
 
     public enum CNItemTags {
+        CLOTH,
+        FUEL,
+        COOLER,
+        URANIUM_ORES,
+        LEAD_ORES
         ;
 
         public final TagKey<Item> tag;
@@ -153,9 +175,103 @@ public class CNTags {
         private static void init() {}
     }
 
+    public enum CNFluidTags {
+        URANIUM,
+        LAVA(MINECRAFT)
+        ;
+
+        public final TagKey<Fluid> tag;
+        public final boolean alwaysDatagen;
+
+        CNFluidTags() {
+            this(MOD);
+        }
+
+        CNFluidTags(NameSpace namespace) {
+            this(namespace, namespace.optionalDefault, namespace.alwayDatagenDefault);
+        }
+
+        CNFluidTags(NameSpace nameSpace, String path) {
+            this(nameSpace, path, nameSpace.optionalDefault, nameSpace.alwayDatagenDefault);
+        }
+
+        CNFluidTags(NameSpace nameSpace, boolean optional, boolean alwayDatagenDefault) {
+            this(nameSpace, null, optional, alwayDatagenDefault);
+        }
+
+        CNFluidTags(NameSpace nameSpace, String path, boolean optional, boolean alwayDatagenDefault) {
+            ResourceLocation id = new ResourceLocation(nameSpace.id, path == null ? Lang.asId(name()) : path);
+            if (optional) {
+                tag = optionalTag(ForgeRegistries.FLUIDS, id);
+            } else {
+                tag = FluidTags.create(id);
+            }
+            this.alwaysDatagen = alwayDatagenDefault;
+        }
+
+        @SuppressWarnings("deprecation")
+        public boolean matches(Fluid fluid) {
+            return fluid.is(tag);
+        }
+
+        public boolean matches(FluidState stack) {
+            return stack.is(tag);
+        }
+
+        private static void init() {}
+    }
+
+    public enum CNEntityTags {
+        FALL_DAMAGE_IMMUNE(MINECRAFT, "fall_damage_immune"),
+        IRRADIATED_IMMUNE
+        ;
+
+        public final TagKey<EntityType<?>> tag;
+        public final boolean alwaysDatagen;
+
+        CNEntityTags() {
+            this(MOD);
+        }
+
+        CNEntityTags(NameSpace nameSpace) {
+            this(nameSpace, nameSpace.optionalDefault, nameSpace.alwayDatagenDefault);
+        }
+
+        CNEntityTags(NameSpace nameSpace, String path) {
+            this(nameSpace, path, nameSpace.optionalDefault, nameSpace.alwayDatagenDefault);
+        }
+
+        CNEntityTags(NameSpace nameSpace, boolean optional, boolean alwayDatagenDefault) {
+            this(nameSpace, null, optional, alwayDatagenDefault);
+        }
+
+        CNEntityTags(NameSpace nameSpace, String path, boolean optional, boolean alwayDatagenDefault) {
+            ResourceLocation id = new ResourceLocation(nameSpace.id, path == null ? Lang.asId(name()) : path);
+            if (optional) {
+                tag = optionalTag(ForgeRegistries.ENTITY_TYPES, id);
+            } else {
+                tag = TagKey.create(Registries.ENTITY_TYPE, id);
+            }
+            this.alwaysDatagen = alwayDatagenDefault;
+        }
+
+        public boolean matches(EntityType<?> type) {
+            return type.is(tag);
+        }
+
+        public boolean matches(Entity entity) {
+            return matches(entity.getType());
+        }
+
+        private static void init() {}
+    }
+
 
     public static void init() {
+        CreateNuclear.LOGGER.info("Registering mod tags for " + CreateNuclear.MOD_ID);
         CNBlockTags.init();
         CNItemTags.init();
+        CNFluidTags.init();
+        CNEntityTags.init();
     }
 }

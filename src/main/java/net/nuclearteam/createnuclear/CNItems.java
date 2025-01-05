d3ac060ce3088b72d6977fac53c96337856f1316
Giovanni <giovanni.ricotta@ynov.com>
2025-01-05 14:47:09 +0100

    initial-create-nuclear-forge: Updated add spawn egg

--- src/main/java/net/nuclearteam/createnuclear/CNItems.java
+++ src/main/java/net/nuclearteam/createnuclear/CNItems.java
@@ -1,7 +1,13 @@
 package net.nuclearteam.createnuclear;
 
 import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeSpawnEggItem;
+
+import java.util.function.Supplier;
 
 public class CNItems {
 
@@ -10,6 +16,21 @@ public class CNItems {
             .item("yellowcake", Item::new)
             .register()
     ;
+
+    public static final ItemEntry<ForgeSpawnEggItem> SPAWN_WOLF = registerSpawnEgg("wolf_irradiated_spawn_egg", CNEntityType.IRRADIATED_WOLF, 0x42452B,0x4C422B, "Irradiated Wolf Spawn Egg");
+    public static final ItemEntry<ForgeSpawnEggItem> SPAWN_CAT = registerSpawnEgg("cat_irradiated_spawn_egg", CNEntityType.IRRADIATED_CAT, 0x382C19, 0x742728, "Irradiated Cat Spawn Egg");
+    public static final ItemEntry<ForgeSpawnEggItem> SPAWN_CHICKEN = registerSpawnEgg("chicken_irradiated_spawn_egg", CNEntityType.IRRADIATED_CHICKEN, 0x6B9455, 0x95393C, "Irradiated Chicken Spawn Egg");
+
+
+    private static ItemEntry<ForgeSpawnEggItem> registerSpawnEgg(String name, Supplier<? extends EntityType<? extends Mob>> entity, int backgroundColor, int highlightColor, String nameItems) {
+        return CreateNuclear.REGISTRATE
+                .item(name, p -> new ForgeSpawnEggItem(entity, backgroundColor, highlightColor, p))
+                .lang(nameItems)
+                .model((c, p) -> p.withExistingParent(c.getName(), new ResourceLocation("item/template_spawn_egg")))
+                .register();
+
+    }
+
 
     public static void register() {}
 }

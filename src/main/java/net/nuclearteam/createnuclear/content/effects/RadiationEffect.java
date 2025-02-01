41b90a666b9e580bf7d7d462cb705bffb2a31ca7
Giovanni <giovanni.ricotta@ynov.com>
2025-02-01 13:46:23 +0100

    fixResiteEffectforArmurAndBuild: Updated

--- src/main/java/net/nuclearteam/createnuclear/content/effects/RadiationEffect.java
+++ src/main/java/net/nuclearteam/createnuclear/content/effects/RadiationEffect.java
@@ -1,10 +1,12 @@
 package net.nuclearteam.createnuclear.content.effects;
 
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
 import net.nuclearteam.createnuclear.CNEffects;
 import net.nuclearteam.createnuclear.CNTags;
+import net.nuclearteam.createnuclear.CreateNuclear;
 import net.nuclearteam.createnuclear.content.equipment.armor.AntiRadiationArmorItem;
 
 public class RadiationEffect extends MobEffect {
@@ -22,10 +24,13 @@ public class RadiationEffect extends MobEffect {
         //Si le joueur porte l'armure anti_radiation_suit alors il ne prend pas de dégâts
         livingEntity.getArmorSlots().forEach(e -> {
             //à rajouter quand l'armure sera faite
-            if (livingEntity.hasEffect( CNEffects.RADIATION.get()) && AntiRadiationArmorItem.Armor.isArmored2(e)) {
-                livingEntity.hurt(livingEntity.damageSources().magic(), 0.0F);
+            //CreateNuclear.LOGGER.warn("test: {}, effect: {}, test2: {}, damageType: {}", AntiRadiationArmorItem.Armor.isArmored2(e), livingEntity.hasEffect(CNEffects.RADIATION.get()), livingEntity.hasEffect(CNEffects.RADIATION.get()) && AntiRadiationArmorItem.Armor.isArmored2(e), livingEntity.damageSources());
+            if (livingEntity.hasEffect(CNEffects.RADIATION.get()) && AntiRadiationArmorItem.Armor.isArmored2(e)) {
+                livingEntity.hurt(livingEntity.damageSources().magic(), 0);
+                //livingEntity.removeEffect(this);
+
             }
-            if (livingEntity.getType().is(CNTags.CNEntityTags.IRRADIATED_IMMUNE.tag)) {
+            else if (livingEntity.getType().is(CNTags.CNEntityTags.IRRADIATED_IMMUNE.tag)) {
                 livingEntity.removeEffect(this);
             }
             else {
